1a4555119aa633be567428880e6c7431cd2519ba
Shuva Kharel <shuvakharel007@gmail.com>
2024-11-28 23:54:54 +0545

    Engine Setup Day 1

---
+++ State.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace Shuva {
+	class State {
+	public:
+		virtual void Init() = 0;
+
+		virtual void HandleInput() = 0;
+		virtual void Update() = 0;
+		virtual void Draw(float dt) = 0;
+
+
+		virtual void Pause();
+		virtual void Resume();
+	};
+}
--- main.cpp
+++ main.cpp
@@ -1,21 +1,6 @@
-#include <SFML/Graphics.hpp>
+#include <iostream>
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(1080, 1080), "Flappy Bird Clone - Shuva");
+int main(){
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.display();
-    }
-
-    return 0;
+    return EXIT_SUCCESS;
 }
